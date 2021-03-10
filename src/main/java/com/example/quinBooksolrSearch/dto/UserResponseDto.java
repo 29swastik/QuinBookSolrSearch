ea9eb3f11c524cdf8a37f0df20c4a5f34b4c0003
Swastik N <swastikn@Swastiks-MacBook-Pro.local>
2021-03-10 16:10:34 +0530

    solr final

--- src/main/java/com/example/quinBooksolrSearch/dto/UserResponseDto.java
+++ src/main/java/com/example/quinBooksolrSearch/dto/UserResponseDto.java
@@ -5,8 +5,18 @@ import lombok.Data;
 @Data
 public class UserResponseDto {
 
-    private Long userId;
+    private long userId;
     private String userName;
-    private String email;
-    private String location;
+    private String img;
+    private String relationshipStatus;
+    private String education10;
+    private String education12;
+    private String educationUni;
+    private String jobProfile;
+    private String companyName;
+    private java.sql.Date jobStartDate;
+    private java.sql.Date jobEndDate;
+    private Long yearsOfExp;
+    private String jobLocation;
+    private String address;
 }
--- src/main/java/com/example/quinBooksolrSearch/listener/KafkaConsumer.java
+++ src/main/java/com/example/quinBooksolrSearch/listener/KafkaConsumer.java
@@ -1,15 +1,35 @@
 package com.example.quinBooksolrSearch.listener;
 
+import com.example.quinBooksolrSearch.dto.UserRequestDto;
+import com.example.quinBooksolrSearch.entity.QuinBookUser;
+import com.example.quinBooksolrSearch.service.SearchService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
 
+    @Autowired
+    SearchService searchService;
+
     @KafkaListener(topics = "my-events", groupId = "group_id")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
+        ObjectMapper objectMapper = new ObjectMapper();
+        QuinBookUser quinBookUser = null;
+        try {
+            quinBookUser = objectMapper.readValue(message, QuinBookUser.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
+        UserRequestDto userRequestDto = new UserRequestDto();
+        BeanUtils.copyProperties(quinBookUser, userRequestDto);
+        searchService.addUser(userRequestDto);
     }
 }
 
